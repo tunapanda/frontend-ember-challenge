7cb6e5407479a2f110eb08fc380e78b409138a42
Keithkiarie <keithkiarie@gmail.com>
2019-10-19 10:46:34 +0300

    added action to toggle done-undo text

--- app/components/single-task.js
+++ app/components/single-task.js
@@ -4,6 +4,9 @@ export default Component.extend({
     actions: {
         pin_clicked() {
             this.onSentUp(this.task);
+        },
+        toggle() {
+            this.task.isComplete ? this.task.isComplete = false : this.task.isComplete = true;
         }
     }
 });
