5978e25f16a6ac69e9b709434ed84c8143d82a07
Keithkiarie <keithkiarie@gmail.com>
2021-01-06 18:10:09 +0300

    update completedTasks using compute property

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,10 +2,16 @@ import Controller from '@ember/controller';
 import {
   action
 } from '@ember/object';
+import { computed } from '@ember/object';
 
 export default class IndexController extends Controller {
     @action
     pinned() {
         this.set('pinnedTask', arguments[0]);
+    }
+
+    @computed('model.@each.isComplete')
+    get completedTasks() {
+        return this.model.filterBy('isComplete', true).length;
     }
 }
