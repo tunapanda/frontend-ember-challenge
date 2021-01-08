de077ba24a0581e54ef5c38dad57d03de00936e7
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2021-01-08 15:19:50 +0300

    get done tasks count
    Created a getter method which filters the controller's model and returns the number of tasks that are complete

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,6 +4,10 @@ import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
     @tracked pinnedTask;
+    
+    get completedTasks() {
+        return this.model.filter(task => task.get('isComplete')).length;
+    }
 
     // A function that takes task to be pinned
     @action
