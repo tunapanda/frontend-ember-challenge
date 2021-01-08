ede956dcc9965b6d5824d8f8f1cfa724f864a354
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2021-01-08 15:16:34 +0300

    pin task
    Created an action called pinTask in the index controller and passed it down to task-list component which in turn passes it to its single-task child component.
    On click of the pin button, the single-task component calls the action that was passed down to it by its parent task-list component, providing it with the task. The task-list component in turns calls the
    pinTask in the index controller which updates its pinnedTask value

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,13 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
+    @tracked pinnedTask;
+
+    // A function that takes task to be pinned
+    @action
+    pinTask(task) {
+        this.pinnedTask = task.toJSON();
+    }
 }
