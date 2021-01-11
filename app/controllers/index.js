61a3c3277f60eb79b22563b03f28846721e0c69c
mimidots <murage@labskenya.com>
2021-01-11 06:12:40 +0300

    - Refactored the index template to utilize `pinnedTasks` computed property from the filtered model, leading to re-usability of `TaskList` component. - Introduced computed property for `unpinned tasks` in the index controller.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import {action} from '@ember/object';
+import {filterBy} from '@ember/object/computed';
 
 export default class IndexController extends Controller {
 
@@ -8,5 +9,17 @@ export default class IndexController extends Controller {
   @action
   toggleTaskPin(task) {
     task.toggleProperty('isPinned');
+  }
+
+  @filterBy('model', 'isPinned', true) pinnedTasks; //computed property using filterBy decorator
+
+
+  get unpinnedTasks() {
+    /**
+     * Filter unpinned tasks from tasks model
+     * returns @computed property : all task where isPinned key doesn't exit or it's value is false
+     */
+    return [...this.model.filterBy('isPinned', undefined),
+      ...this.model.filterBy('isPinned', false)];
   }
 }
