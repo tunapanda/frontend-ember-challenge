978156077062934f192bc435145e20b7820321c4
john mutavi <jonnygovish@gmail.com>
2021-01-11 07:06:12 +0300

    Created an action for changing isComplete to true

--- app/components/single-task.js
+++ app/components/single-task.js
@@ -1,7 +1,12 @@
 import Component from '@ember/component';
 import { tagName } from '@ember-decorators/component';
+import { action } from '@ember/object';
 
 export default 
 @tagName('')
 class SingleTaskComponent extends Component {
+    @action 
+    taskComplete(){
+        this.task.isComplete = true;
+    }
 }
