a07474e86fbd433bb4d389bd7bf9867263722842
john mutavi <jonnygovish@gmail.com>
2021-01-11 06:05:01 +0300

    Create an action that gets a task from the single task component and then creates a pinned task object.
    Thought Process:
    As per the instructions, since the action needs to send tasks to the index controller. I decided to define the action  here so that I can pass it down to the single task component. That way it will be able to send the task that is pinned to the index controller.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,15 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
 
 export default class IndexController extends Controller {
+    // Tracking the pinnedTrack object
+    @tracked pinnedTask;
+    
+    // Action that gets a task from the single task component after the user pins the task
+    @action
+    pinTask(task) {
+        this.pinnedTask = task.toJSON();
+    }
 }
