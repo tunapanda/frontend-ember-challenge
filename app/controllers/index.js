443006bc355cebcde4737d30b17f9131b4a02152
mimidots <murage@labskenya.com>
2021-01-11 05:51:47 +0300

    -Added `isPinned` tracked property on the application index route since the controllers are singleton. - Added `togglePin` action on controller to provide a way for user to toggle pinned tasks. (Passed in to component as children attribute)

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,12 @@
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
 
 export default class IndexController extends Controller {
+
+  isPinnedTask=false;
+
+  @action
+  toggleTaskPin(task) {
+    task.toggleProperty('isPinned');
+  }
 }
