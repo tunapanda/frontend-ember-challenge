64ac168d9a80bf44e2473ea68c6dc862d8554761
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-11 08:26:01 +0300

    Set task as pinned

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,12 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 export default class IndexController extends Controller {
+  @tracked pinnedTask;
+
+  @action
+  setTaskAsPinned(task) {
+    this.pinnedTask = task.toJSON();
+  }
 }
