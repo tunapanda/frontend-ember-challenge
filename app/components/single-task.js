84d734af18f8bf18b487c47dd54994c2d7905f8a
yegow <allanyego05@gmail.com>
2021-01-11 05:31:45 +0300

    Add pinning action to Single Task component
    Allows users to toggle the pinned state of each task.

--- app/components/single-task.js
+++ app/components/single-task.js
@@ -1,7 +1,15 @@
-import Component from '@ember/component';
-import { tagName } from '@ember-decorators/component';
+import Component from "@ember/component";
+import { tagName } from "@ember-decorators/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
-export default 
-@tagName('')
+export default
+@tagName("")
 class SingleTaskComponent extends Component {
+  @tracked isPinned = false;
+
+  @action
+  togglePinned() {
+    this.isPinned = !this.isPinned;
+  }
 }
