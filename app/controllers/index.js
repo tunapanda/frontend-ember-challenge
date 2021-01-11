708cc7d941161a2f9c34f4afcd311df91ffd8e19
yegow <allanyego05@gmail.com>
2021-01-11 05:42:48 +0300

    Add completed task count property
    A computed property that tracks the number of completed tasks.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,7 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
 
 export default class IndexController extends Controller {
+  get completedTasks() {
+    return this.model.filter((task) => task.isComplete).length;
+  }
 }
