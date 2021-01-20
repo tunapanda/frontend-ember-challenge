040b16c41615835abb2600ff15c60dd1054c2b48
john mutavi <jonnygovish@gmail.com>
2021-01-20 23:50:03 +0300

    Toggle Complete for Single Task Component
    Create the action for toggling the status of the complete task

--- app/components/single-task.js
+++ app/components/single-task.js
@@ -5,8 +5,10 @@ import { action } from '@ember/object';
 export default 
 @tagName('')
 class SingleTaskComponent extends Component {
+
+    // Action for toggling the isComplete property between false and true
     @action 
     taskComplete(){
-        this.task.isComplete = true;
+        this.task.isComplete = !this.task.isComplete
     }
 }
